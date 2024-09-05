52593c30382d46935ccec38b4cb9f2e54c356ea2
junayedislam12 <mdjonayedislam5@gmail.com>
2024-09-06 00:13:44 +0600

    input

---
+++ bin/if-else-for-statement.dart
@@ -0,0 +1,13 @@
+main() {
+  double weather = 40;
+  if (weather > 50) {
+    print('Enjoying Movie');
+  } else if (weather >= 26.3 && weather <= 30) {
+    print('Going For Work');
+  } else if (weather == 22) {
+    print('TAke Tea');
+  }
+else{
+  print('need to bed rest');
+}
+}
